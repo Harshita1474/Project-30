a7a580158374c659cdc6357b3127a341d3214917
Harshita1474 <72028497+Harshita1474@users.noreply.github.com>
2020-10-20 15:56:39 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -18,21 +18,17 @@ class Block{
   display(){
     var pos = this.body.position; 
     
-    if(this.body.speed < 3){ 
-      push(); 
-      translate(pos.x, pos.y);  
-      strokeWeight(3);
-      stroke("white")
-      rectMode(CENTER); 
-      rect(0,0,this.width, this.height); 
-      pop(); 
-    } else{ 
-      World.remove(world, this.body); 
-      push(); 
-      this.visibility = this.visibility - 5; 
-      tint(255,this.visibility); 
-      pop(); 
-    }
+    if(this.body.speed < 3){
+    image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+   } 
 
   }
 
