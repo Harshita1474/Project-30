738891170a34aaaee16dc697d26f061b4af1df5a
Harshita1474 <72028497+Harshita1474@users.noreply.github.com>
2020-10-20 19:56:30 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -17,19 +17,15 @@ class Block{
 
   display(){
     var pos = this.body.position; 
+    rect(this.body.position.x,this.body.position.y,this.width,this.height);
     
-    if(this.body.speed < 3){
-    image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
-   }
-   else{
-     World.remove(world, this.body);
+    if(this.body.speed > 3){
+    World.remove(world, this.body);
      push();
      this.Visiblity = this.Visiblity - 5;
      tint(255,this.Visiblity);
-     image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+     rect(this.body.position.x, this.body.position.y, this.width, this.height);
      pop();
-   } 
-
   }
 
  
